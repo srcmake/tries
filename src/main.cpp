b23deb0059846e3dfea10caa1914cacae6662081
Tariq Ramlall <srcmake@gmail.com>
2020-04-05 17:16:34 -0400

    Renamed Search -> Contain and filled code in. Test passes.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 // Copyright srcmake.com 2020.
 #include <iostream>
+#include <assert.h>
 #include "trie.hpp"
 using namespace std;
   
@@ -8,9 +9,17 @@ int main()
 	cout << "Program started.\n";
 
 	Trie trie;
+	
 	trie.Insert("apple");
 	trie.Insert("banana");
 	trie.Insert("apricot");
+
+	assert(trie.Contains("apple") == true);
+	assert(trie.Contains("app") == false);
+	assert(trie.Contains("apricot") == true);
+
+	assert(trie.Contains("bananas") == false);
+	assert(trie.Contains("banana") == true);
 	
 	cout << "Program ended.\n";
 	return 0;
--- src/trie.hpp
+++ src/trie.hpp
@@ -13,7 +13,7 @@ class Trie
 		Trie();
 		~Trie();
 		void Insert(const std::string&);
-		bool Search(const std::string&);
+		bool Contains(const std::string&);
 		bool StartsWith(const std::string&);
 	};
 ///////////////////////////////////////
@@ -60,7 +60,7 @@ Trie::~Trie()
 ///////////////////////////////////////
 void Trie::Insert(const std::string& word)
 	{
-	std::cout << "Inserting word " << word << std::endl;
+	//std::cout << "Inserting word " << word << std::endl;
 
 	// Starting from the root, traverse/create nodes for each char in the string.
 	Node* curr = root;
@@ -71,38 +71,68 @@ void Trie::Insert(const std::string& word)
 		// Check if this char has a node created for it yet.
 		std::unordered_map<char, Node*>::const_iterator it = curr->nextNodesMap.find(c);
 		
-		// The node didn't exist so create it.
+		// The node doesn't exist so create it.
 		Node* node;
 		if(it == curr->nextNodesMap.end())
 			{
 			node = new Node(c);
 			curr->nextNodesMap[c] = node;
 			}
-		// The node did exist so make "node" point to it.
+		// The node does exist so make "node" point to it.
 		else
 			{
 			node = curr->nextNodesMap[c];
 			}
-		
-
+	
 		// If this is the last character in the word, mark the node as the end of the word.
 		if(i == word.length() - 1)
 			{
 			node->isEndOfWord = true;
 			}
 
-		// Update curr to pointer.
+		// Update curr.
 		curr = node;
 		}
 
-	std::cout << "Finished inserting word " << word << std::endl;
+	//std::cout << "Finished inserting word " << word << std::endl;
 	}
 ///////////////////////////////////////
 
 
 ///////////////////////////////////////
-bool Trie::Search(const std::string&)
+bool Trie::Contains(const std::string& word)
 	{
+	// Starting from the root, traverse/create nodes for each char in the string.
+	Node* curr = root;
+	for(int i = 0; i < word.length(); i++)
+		{
+		char c = word[i];
+
+		// Check if this char has a node created for it yet.
+		std::unordered_map<char, Node*>::const_iterator it = curr->nextNodesMap.find(c);
+		
+		// The node doesn't exist so this word wassn't inserted.
+		Node* node;
+		if(it == curr->nextNodesMap.end())
+			{
+			return false;
+			}
+		// The node does exist so make "node" point to it.
+		else
+			{
+			node = curr->nextNodesMap[c];
+			}
+	
+		// If this is the last character in the word, check if it's marked as the return of a word.
+		if(i == word.length() - 1)
+			{
+			return node->isEndOfWord;
+			}
+
+		// Update curr.
+		curr = node;
+		}
+	
 	return false;
 	}
 ///////////////////////////////////////
